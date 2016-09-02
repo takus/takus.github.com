dfdf8351b0cdea4f06faf82c0c0802e2ac6ad9b8
Takumi Sakamoto <takumi.saka@gmail.com>
2016-09-03 01:25:03 +0900

    Edit hugo release URL

--- ci/setup.sh
+++ ci/setup.sh
@@ -10,7 +10,7 @@ CACHED_DOWNLOAD="${HOME}/cache/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
 mkdir -p "${HUGO_DIR}"
 wget --continue \
 	--output-document "${CACHED_DOWNLOAD}" \
-	"https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
+	"https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-64bit.tgz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${HUGO_DIR}"
 ln -s "${HUGO_DIR}/hugo_${HUGO_VERSION}_linux_amd64" "${HOME}/bin/hugo"
 
