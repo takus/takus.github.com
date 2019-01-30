fb37f7580198c27da4051b150050bed62c960685
Takumi Sakamoto <takumi.saka@gmail.com>
2019-01-30 22:35:09 +0900

    Upgrade hugo to v0.53

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Install a Hugo, https://gohugo.io/
 
-HUGO_VERSION="0.16"
+HUGO_VERSION="0.53"
 HUGO_DIR=${HUGO_DIR:="$HOME/hugo"}
 
 set -e
@@ -10,7 +10,7 @@ CACHED_DOWNLOAD="${HOME}/cache/hugo_${HUGO_VERSION}_linux-64bit.tgz"
 mkdir -p "${HUGO_DIR}"
 wget --continue \
 	--output-document "${CACHED_DOWNLOAD}" \
-	"https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-64bit.tgz"
+	"https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-64bit.tgz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${HUGO_DIR}"
 ln -s "${HUGO_DIR}/hugo" "${HOME}/bin/hugo"
 
