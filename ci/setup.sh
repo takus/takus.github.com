15b275a5dcdb4575cbf8f9c35fb4eaf1fd46efb2
Takumi Sakamoto <takumi.saka@gmail.com>
2019-01-30 22:50:47 +0900

    Upgrade hugo to v0.53

--- ci/setup.sh
+++ ci/setup.sh
@@ -11,7 +11,7 @@ mkdir -p "${HUGO_DIR}"
 wget --continue \
 	--output-document "${CACHED_DOWNLOAD}" \
 	"https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-64bit.tar.gz"
-tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${HUGO_DIR}"
+tar xvzf "${CACHED_DOWNLOAD}" -C "${HUGO_DIR}"
 ln -s "${HUGO_DIR}/hugo" "${HOME}/bin/hugo"
 
 hugo version
